3eabc09dc39c01d04756c1c55621ffdc49c1be1b
unknown <Danilo>
2019-09-26 17:12:21 -0300

    feat: added redux, user reducer and user actions to header and app components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import './index.css'
 import App from './App'
 
+import store from './redux/store'
+
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
 
-    <App />
+    <BrowserRouter>
 
-  </BrowserRouter>, document.getElementById('root')
+      <App />
+
+    </BrowserRouter>
+
+  </Provider>, document.getElementById('root')
 )
